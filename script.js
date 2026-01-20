1ecb3b798f4b11d9e582a8b75c02e522e85bc0c9
jesuslerdo2001 <jesuslerdo620@gmail.com>
2026-01-20 00:40:49 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAT8FLvXeSSXXqvGnwHm678GfZWKfBC4tM",
+  authDomain: "huella-segura-ef4dd.firebaseapp.com",
+  databaseURL: "https://huella-segura-ef4dd-default-rtdb.firebaseio.com",
+  projectId: "huella-segura-ef4dd",
+  storageBucket: "huella-segura-ef4dd.firebasestorage.app",
+  messagingSenderId: "508657691401",
+  appId: "1:508657691401:web:a61bf8e85b312d1a7181d8"
+};
+
+const app = initializeApp(firebaseConfig);
